83a0d1715338f3850316117a00e63d28004ecd29
leauxgan1 <logan.langdon@yahoo.com>
2022-01-19 00:52:00 -0800

    State-Based Movement, Ground Tag
    Added a state system to movement to control different modes of movement
    Also added a Ground tag to check when the player is touching the ground after a jump.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,14 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PlayerController : MonoBehaviour
 {
     private GameObject self;
     private GameObject Body;
     private Rigidbody BodyPhysics;
     public float speed = 0.5f;
+    public float jumpForce = 1.0f;
     public float maximumSpeed = 10.0f;
-    // Start is called before the first frame update
+    public float groundDistance = 0.1f;
+    public enum State {
+        Grounded,
+        Jumping
+    }
+    private State state;
+
     void Start()
     {
         self = gameObject;
@@ -17,35 +26,69 @@ public class PlayerController : MonoBehaviour
         BodyPhysics = Body.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        /**/
+        moveHorizontal();
         
-        float xMagnitude = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        float zMagnitude = Input.GetAxis("Vertical") * Time.deltaTime * speed;
-        BodyPhysics.AddRelativeForce(xMagnitude, 0.0f, zMagnitude, ForceMode.Impulse);
-        constrainSpeed();
-        /*
-        if(Input.GetKey(KeyCode.W)) {
-             transform.position += Body.transform.forward * Time.deltaTime * speed;
-         }
-         else if(Input.GetKey(KeyCode.S)) {
-             transform.position -= Body.transform.forward * Time.deltaTime * speed;
-         }
-         else if(Input.GetKey(KeyCode.A)) {
-             transform.position -= Body.transform.right * Time.deltaTime * speed;
-         }
-         else if(Input.GetKey(KeyCode.D)) {
-             transform.position += Body.transform.right * Time.deltaTime * speed;
-         }
-         //*/
     }
 
-    void constrainSpeed() {
-        if(BodyPhysics.velocity.magnitude > maximumSpeed) {
-            Vector3 newVel = Vector3.ClampMagnitude(BodyPhysics.velocity, maximumSpeed);
-            BodyPhysics.velocity = newVel;
+    void FixedUpdate()  {
+        moveVertical(); 
+    }
+
+    void moveHorizontal() {
+        float xMagnitude = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float zMagnitude = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        if(state == State.Grounded) {
+            BodyPhysics.AddRelativeForce(xMagnitude, 0.0f, zMagnitude, ForceMode.Impulse);
+            /*
+            if(Input.GetKey(KeyCode.W)) {
+                transform.position += Body.transform.forward * Time.deltaTime * speed;
+            }
+            else if(Input.GetKey(KeyCode.S)) {
+                transform.position -= Body.transform.forward * Time.deltaTime * speed;
+            }
+            else if(Input.GetKey(KeyCode.A)) {
+                transform.position -= Body.transform.right * Time.deltaTime * speed;
+            }
+            else if(Input.GetKey(KeyCode.D)) {
+                transform.position += Body.transform.right * Time.deltaTime * speed;
+            }
+            //*/
+        }
+        else if(state == State.Jumping) {
+            //Set the acceleration in the player's movement direction
+            resetState();
+        }
+        constrainHorizontalSpeed();
+    }
+
+    void constrainHorizontalSpeed() {
+        if(state == State.Grounded) {
+            if(BodyPhysics.velocity.magnitude > maximumSpeed) {
+                Vector3 newVel = Vector3.ClampMagnitude(BodyPhysics.velocity, maximumSpeed);
+                BodyPhysics.velocity = newVel;
+            }
+        }
+        else if(state == State.Jumping) {
+
+        }
+        
+    }
+
+    void moveVertical() {
+        if(Input.GetKey(KeyCode.Space) && state != State.Jumping) {
+            state = State.Jumping;
+            Debug.Log("Jumping");
+            BodyPhysics.AddRelativeForce(0.0f, jumpForce, 0.0f, ForceMode.Impulse);
+        }
+    }
+
+    void resetState() {
+        float distToGround = Body.GetComponent<Collider>().bounds.extents.y;
+        if(Physics.Raycast(Body.transform.position, -Vector3.up,distToGround + groundDistance)) {
+            Debug.Log("Grounded");
+            state = State.Grounded; 
         }
     }
 }
